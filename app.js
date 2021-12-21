62b13fe4e7ddc1c6d95b755558207dd5f698643d
salt57 <sourishgupta02@gmail.com>
2021-12-21 18:15:39 +0530

    feat: implemented key gen and exchange

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
-const express = require('express')
+const express = require("express");
 
 // Setup Express server
-const app = express()
-const http = require('http').Server(app)
+const app = express();
+const http = require("http").Server(app);
 
 // Attach Socket.io to server
-const io = require('socket.io')(http)
+const io = require("socket.io")(http);
 
 // Serve web app directory
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // INSERT SOCKET.IO CODE HERE
 /** Manage behavior of each client socket connection */
-io.on('connection', (socket) => {
-    console.log(`User Connected - Socket ID ${socket.id}`)
-  
-    // Store the room that the socket is connected to
-    let currentRoom = 'DEFAULT'
-  
-    /** Process a room join request. */
-    socket.on('JOIN', (roomName) => {
-      socket.join(currentRoom)
-  
-      // Notify user of room join success
-      io.to(socket.id).emit('ROOM_JOINED', currentRoom)
-  
-      // Notify room that user has joined
-      socket.broadcast.to(currentRoom).emit('NEW_CONNECTION', null)
-    })
-  
-    /** Broadcast a received message to the room */
-    socket.on('MESSAGE', (msg) => {
-      console.log(`New Message - ${msg}`)
-      socket.broadcast.to(currentRoom).emit('MESSAGE', msg)
-    })
-  })
+io.on("connection", (socket) => {
+  console.log(`User Connected - Socket ID ${socket.id}`);
+
+  // Store the room that the socket is connected to
+  let currentRoom = "DEFAULT";
+
+  /** Process a room join request. */
+  socket.on("JOIN", (roomName) => {
+    socket.join(currentRoom);
+
+    // Notify user of room join success
+    io.to(socket.id).emit("ROOM_JOINED", currentRoom);
+
+    // Notify room that user has joined
+    socket.broadcast.to(currentRoom).emit("NEW_CONNECTION", null);
+  });
+
+  /** Broadcast a received message to the room */
+  socket.on("MESSAGE", (msg) => {
+    console.log(`New Message - ${msg}`);
+    socket.broadcast.to(currentRoom).emit("MESSAGE", msg);
+  });
+
+  /** Broadcast a new publickey to the room */
+  socket.on("PUBLIC_KEY", (key) => {
+    socket.broadcast.to(currentRoom).emit("PUBLIC_KEY", key);
+  });
+
+  /** Broadcast a disconnection notification to the room */
+  socket.on("disconnect", () => {
+    socket.broadcast.to(currentRoom).emit("USER_DISCONNECTED", null);
+  });
+});
 
 // Start server
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 http.listen(port, () => {
-  console.log(`Chat server listening on port ${port}.`)
-})
+  console.log(`Chat server listening on port ${port}.`);
+});
--- public/page.js
+++ public/page.js
@@ -14,8 +14,17 @@ const vm = new Vue({
       draft: "",
     };
   },
-  created() {
-    // Initialize socket.io
+  async created() {
+    this.addNotification("Welcome! Generating a new keypair now.");
+
+    // Initialize crypto webworker thread
+    this.cryptWorker = new Worker("crypto-worker.js");
+
+    // Generate keypair and join default room
+    this.originPublicKey = await this.getWebWorkerResponse("generate-keys");
+    this.addNotification("Keypair Generated");
+
+    // Initialize socketio
     this.socket = io();
     this.setupSocketListeners();
   },
@@ -41,6 +50,35 @@ const vm = new Vue({
       // Display message when recieved
       this.socket.on("MESSAGE", (message) => {
         this.addMessage(message);
+      });
+
+      // When a user joins the current room, send them your public key
+      this.socket.on("NEW_CONNECTION", () => {
+        this.addNotification("Another user joined the room.");
+        this.sendPublicKey();
+      });
+
+      // Broadcast public key when a new room is joined
+      this.socket.on("ROOM_JOINED", (newRoom) => {
+        this.currentRoom = newRoom;
+        this.addNotification(`Joined Room - ${this.currentRoom}`);
+        this.sendPublicKey();
+      });
+
+      // Save public key when received
+      this.socket.on("PUBLIC_KEY", (key) => {
+        this.addNotification(
+          `Public Key Received - ${this.getKeySnippet(key)}`
+        );
+        this.destinationPublicKey = key;
+      });
+
+      // Clear destination public key if other user leaves room
+      this.socket.on("user disconnected", () => {
+        this.notify(
+          `User Disconnected - ${this.getKeySnippet(this.destinationKey)}`
+        );
+        this.destinationPublicKey = null;
       });
     },
 
@@ -71,6 +109,44 @@ const vm = new Vue({
     /** Add message to UI */
     addMessage(message) {
       this.messages.push(message);
+    },
+    /** Post a message to the web worker and return a promise that will resolve with the response.  */
+    getWebWorkerResponse(messageType, messagePayload) {
+      return new Promise((resolve, reject) => {
+        // Generate a random message id to identify the corresponding event callback
+        const messageId = Math.floor(Math.random() * 100000);
+
+        // Post the message to the webworker
+        this.cryptWorker.postMessage(
+          [messageType, messageId].concat(messagePayload)
+        );
+
+        // Create a handler for the webworker message event
+        const handler = function (e) {
+          // Only handle messages with the matching message id
+          if (e.data[0] === messageId) {
+            // Remove the event listener once the listener has been called.
+            e.currentTarget.removeEventListener(e.type, handler);
+
+            // Resolve the promise with the message payload.
+            resolve(e.data[1]);
+          }
+        };
+
+        // Assign the handler to the webworker 'message' event.
+        this.cryptWorker.addEventListener("message", handler);
+      });
+    },
+    /** Emit the public key to all users in the chatroom */
+    sendPublicKey() {
+      if (this.originPublicKey) {
+        this.socket.emit("PUBLIC_KEY", this.originPublicKey);
+      }
+    },
+
+    /** Get key snippet for display purposes */
+    getKeySnippet(key) {
+      return key.slice(400, 416);
     },
   },
 });
